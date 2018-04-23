8f0dd033d80301ce69bcfc163dc4ba61c428e0fb
PrPopCorn <burnt.crepe@gmail.com>
2018-04-22 21:13:13 -0400

    Scheduler architecture

--- fleet.py
+++ fleet.py
@@ -3,3 +3,9 @@ driver = None
 #TODO: spy
 #TODO: attack
 #TODO: transport
+
+
+class FleetScheduler():
+
+    def __init__(self, goals):
+        self.goals = goals
--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,32 @@
+import planet
+import research
+import buildings
+import fleet
+
+
 #TODO
+
+
+class MasterScheduler():
+
+
+    def __init__(self, configFile):
+        self.empire = planet.Empire()
+        self.buildingSchedulers = self.init_building_schedulers(configFile)
+        self.researchScheduler = self.init_research_scheduler(configFile)
+        self.fleetScheduler = self.init_fleet_scheduler(configFile)
+        self.events = []
+
+    def init_building_schedulers(self, configFile):
+        #TODO: read goals from config
+        schedulers = [buildings.BuildingScheduler(planet, []) for planet in self.empire.planets.values()]
+        return schedulers
+
+    def init_research_scheduler(self, configFile):
+        #TODO: read goals from config
+        researchPlanet = self.empire.planets['HomeWorld']
+        return research.ResearchScheduler(researchPlanet, [])
+
+    def init_fleet_scheduler(self, configFile):
+        #TODO: read goals from config
+        return fleet.FleetScheduler([])
